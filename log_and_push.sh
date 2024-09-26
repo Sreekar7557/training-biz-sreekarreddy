3dc9a57899d1e2431f04186badc96274ac28b85b
Cloud User <ec2-user@ip-172-31-25-212.eu-north-1.compute.internal>
2024-09-26 09:50:45 +0000

    2024-09-26 09:50:45

--- log_and_push.sh
+++ log_and_push.sh
@@ -4,10 +4,10 @@
 cd  /home/ec2-user/training-biz-sreekarreddy
 
 # Log the history of commands to a file
-history > command_history.txt
+history > /home/ec2-user/training-biz-sreekarreddy/ command_history.txt
 
 # Add the file to git
-git add command_history.txt
+git add .
 
 # Commit the file with the current timestamp as the commit message
 git commit -m "$(date +'%Y-%m-%d %H:%M:%S')"
